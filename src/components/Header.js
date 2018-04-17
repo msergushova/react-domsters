8f7d013119c3f7a0c404b310dce5d1cfe9adb25e
Maria Sergushova <mariasergushova@sldrgrl.local>
2018-04-17 16:47:56 -0700

    create SPA domsters

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './../img/logo.gif';
+const Header = () => {
+    return (
+        <div id="header">
+        <img src={logo} alt="Jay Skript and the Domsters"/>
+        </div>
+    )
+}
+export default Header;
